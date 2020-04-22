4c9e1169c7945eea853540d7398d5bd6cfbcacad
lizcramerfox <ecfox2008@gmail.com>
2020-04-22 14:03:56 -0400

    Added entries/ui.js handlers w/ handlebars
    All changes in ui.js
    
    Co-authored-by: Mike Tripp <mctripp94@gmail.com>
    Co-authored-by: Thanh Nguyen <tnansci@gmail.com>
    Co-authored-by: Angelique Marquina <aeamarquina@gmail.com>

--- assets/scripts/entries/ui.js
+++ assets/scripts/entries/ui.js
@@ -1,35 +1,65 @@
 'use strict'
 
 const store = require('./../store.js')
-const getFormFields = require('./../../../lib/get-form-fields.js')
+const useEntriesHandlerTemplate = require('./../templates/entries.handlebars')
+const useEntryHandlerTemplate = require('./../templates/entry.handlebars')
 
 // SUCCESSES --------------------------
 
 // When showing entries is successful:
 const onShowEntriesSuccess = responseData => {
-} // onShowEntriesSuccess
+  // Success-Failure Messages
+  const viewEntriesHtml = useEntriesHandlerTemplate({ entries: responseData.entries })
+  $('#entry-viewer').html(viewEntriesHtml)
+  $('#entry-viewer').removeClass()
+  $('input[class=clear-on-submit]').val('')
+}
 
 // When get entry is success:
 const onGetEntrySuccess = responseData => {
-} // onGetEntrySuccess
-
+  const viewEntryByIdHtml = useEntryHandlerTemplate({ entry: responseData.entry })
+  $('#entry-viewer').html(viewEntryByIdHtml)
+  $('#entry-message').empty()
+  $('input[class=clear-on-submit]').val('')
+}
 // When create entry is success:
 const onCreateEntrySuccess = responseData => {
-} // onCreateEntrySuccess
+  const createNewEntryHtml = useEntryHandlerTemplate({ entry: responseData.entry })
+  $('#entry-viewer').html(createNewEntryHtml)
+  $('#entry-message').text('Entry successfully created.')
+  $('#entry-message').removeClass()
+  $('#entry-message').addClass('success')
+  $('input[class=clear-on-submit]').val('')
+}
 
 // FAILURES -------------------------
 
 // When showing entries is failure:
-const onShowEntriesFailure = responseData => {
-} // onShowEntriesSuccess
+const onShowEntriesFailure = error => {
+  $('#entry-message').text(error.status + ': ' + error.statusText)
+  $('#entry-message').removeClass()
+  $('#entry-message').addClass('failure')
+  $('#entry-viewer').empty()
+  $('input[class=clear-on-submit]').val('')
+}
 
 // When get entry is failure:
-const onGetEntryFailure = responseData => {
-} // onGetEntrySuccess
+const onGetEntryFailure = error => {
+  $('#entry-message').text(error.status + ': ' + error.statusText)
+  $('#entry-message').removeClass()
+  $('#entry-message').addClass('failure')
+  $('#entry-viewer').empty()
+  $('input[class=clear-on-submit]').val('')
+}
 
 // When create entry is failure:
-const onCreateEntryFailure = responseData => {
-} // onCreateEntrySuccess
+const onCreateEntryFailure = error => {
+  $('#entry-message').text(error.status + ': ' + error.statusText)
+  $('#entry-message').removeClass()
+  $('#entry-message').addClass('failure')
+  $('#entry-viewer').empty()
+  $('input[class=clear-on-submit]').val('')
+}
 
 module.exports = {
   onShowEntriesSuccess,
