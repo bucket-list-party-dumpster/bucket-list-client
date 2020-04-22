3ce6eed175de2d4d7ec115a37e9f37d46a32c4e8
Mctripp <mctripp94@gmail.com>
2020-04-22 11:38:14 -0400

    Add auth click events to app.js
    Related to #22

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,14 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const authEvents = require('./auth/event.js')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('click', authEvents.onSignUp)
+  $('#sign-in').on('click', authEvents.onSignIn)
+  $('#change-pw').on('click', authEvents.onChangePassword)
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
